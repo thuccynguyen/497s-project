9ad3fd53d799eb94d00dc2923df034ef1a0c047e
Thuccy Nguyen <thuccynguyen@gmail.com>
2021-11-10 21:45:12 -0500

    added likes microservice

---
+++ likes/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+posts = {};
+posts[7233] = {
+  pid: 7233,
+  liked: false,
+  total_likes: 3,
+};
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  console.log("Event Received:", req.body.type);
+
+  if (type === "PostCreated") {
+    const id = data;
+    posts[id] = { pid: id, liked: false, total_likes: 0 };
+  }
+
+  
+
+  res.send(posts);
+});
+
+app.post("/posts/:id/like", async (req, res) => {
+  if (!posts[req.params.id].liked) {
+    posts[req.params.id].liked = true;
+    posts[req.params.id].total_likes++;
+
+    await axios.post("http://localhost:4006/events", {
+      type: "PostLiked",
+      data: {
+        id: req.params.id,
+        liked: posts[req.params.id].liked,
+        total_likes: posts[req.params.id].total_likes,
+      },
+    });
+
+    res.status(200).send(posts[req.params.id]);
+  } else res.status(403).send("You already liked the post!");
+});
+
+app.listen(4006, () => {
+  console.log("Listening on 4006");
+});
