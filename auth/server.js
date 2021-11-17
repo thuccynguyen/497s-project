563f8d52ecdc52dd243c9d343a82bbccea805714
unknown <zsimonelli@umass.edu>
2021-11-17 17:08:18 -0500

    progress on integrating auth with eventbus

--- auth/server.js
+++ auth/server.js
@@ -1,42 +1,62 @@
-const express = require('express')
-const app = express()
-const bcrypt = require('bcrypt')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bcrypt = require('bcrypt');
+const axios = require("axios");
 
+app.use(cors());
 app.use(express.json())
 
-const users = []
+
+const users = [];
 
 app.get('/users', (req, res) => {
-  res.json(users)
-  res.status(200).send()
+  res.json(users);
+  res.status(200).send();
 })
 
 app.post('/user', async (req, res) => {
   try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    const user = { username: req.body.username, password: hashedPassword }
-    users.push(user)
-    res.status(201).send()
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = { username: req.body.username, password: hashedPassword };
+    users.push(user);
+    res.status(201).send();
   } catch {
-    res.status(500).send()
+    res.status(500).send();
   }
 })
 
 app.post('/users/login', async (req, res) => {
   const user = users.find(user => user.username === req.body.username)
   if (user == null) {
-    return res.status(400).send('Cannot find user')
+    return res.status(400).send('Cannot find user');
   }
   try {
-    if(await bcrypt.compare(req.body.password, user.password)) {
-      res.send('Success')
+    if (await bcrypt.compare(req.body.password, user.password)) {
+
+      res.send('Success');
     } else {
-      res.send('Not Allowed')
+      res.send('Not Allowed');
     }
   } catch {
-    res.status(500).send()
+    res.status(500).send();
   }
-})
+      await axios.post('http://localhost:5000/events', {
+        type: 'UserAuth',
+        data: {
+          id: commentId,
+          user: username,
+          password: hashedPassword
+        }
+      });
 
-app.listen(3000)
+});
+
+app.post('/events', (req, res) => {
+  console.log('Event Received:', req.body.type);
+
+  res.send({});
+});
+
+app.listen(4000);
 
